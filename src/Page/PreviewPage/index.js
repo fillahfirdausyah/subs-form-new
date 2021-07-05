e079d7da992217072c3f8ff20056a2295f693536
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-07-05 16:09:39 +0700

    Make Preview Page

--- src/Page/PreviewPage/index.js
+++ src/Page/PreviewPage/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { database } from "../../firebase";
+import { useAuth } from "../../Context/AuthContext";
+
+// Component
+import Template from "../../Component/Template";
 
 function PreviewPage() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [data, setData] = useState([]);
+  const { id } = useParams();
+
+  const { currentUser } = useAuth();
+
+  useEffect(() => {
+    let ref = database.ref(`/data/${currentUser.uid}/${id}`);
+    ref.on("value", (snap) => {
+      setData([snap.val()]);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Template data={data} id={id} />
+    </div>
+  );
 }
 
-export default PreviewPage
+export default PreviewPage;
