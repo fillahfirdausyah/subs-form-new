be7863bdd361cc0b981629ecb5620465ee43ffcc
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-07-05 12:51:54 +0700

    add marketing page

---
+++ src/Page/MarketingPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Component
+import Dashboard from "../../Component/Marketing/Dashboard";
+
+function MarketingPage() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default MarketingPage;
