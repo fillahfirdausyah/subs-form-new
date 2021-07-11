4f6b5bce12cdb0820b41e008bc55b30be707ec0b
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-07-11 21:13:14 +0700

    displaying data for edit to InstallationAddress Component

--- src/Component/Client/FormEdit/parts/InstallationAddress/index.js
+++ src/Component/Client/FormEdit/parts/InstallationAddress/index.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { database } from "../../../../../firebase";
+import { useAuth } from "../../../../../Context/AuthContext";
 
 // Icon
 import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
@@ -7,9 +9,18 @@ import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
 // Component
 import FormWrapper from "../FormWrapper";
 
-function InstallationAddres({ getInstallationAddress }) {
+function InstallationAddres({ getInstallationAddress, id }) {
+  const { currentUser } = useAuth();
   const [open, setOpen] = useState(false);
   const [data, setData] = useState({});
+
+  useEffect(() => {
+    let ref = database.ref(`data/${currentUser.uid}/${id}`);
+    ref.on("value", (snap) => {
+      let theData = snap.val();
+      setData(theData.installationAddress);
+    });
+  }, []);
 
   const changeHandler = (e) => {
     const { name, value } = e.target;
@@ -45,7 +56,7 @@ function InstallationAddres({ getInstallationAddress }) {
                 id="alamat"
                 rows="3"
                 name="installationAddress"
-                value={data.alamat}
+                value={data.installationAddress}
               ></textarea>
             </div>
           </div>
