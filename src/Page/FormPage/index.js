15d25354a7b9dae3d40d02affc384374d8d8a941
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-07-14 13:03:29 +0700

    add stored image to storage firebase

--- src/Page/FormPage/index.js
+++ src/Page/FormPage/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { database } from "../../firebase";
+import { database, storage } from "../../firebase";
 import { useAuth } from "../../Context/AuthContext";
 
 // Component
@@ -13,10 +13,14 @@ function FormPage() {
   const postHandler = async (data) => {
     try {
       await database.ref("data/" + currentUser.uid).push(data);
+      let storageRef = storage.ref();
+      const fileRef = storageRef.child(`images/${data.client.ttd.name}`);
+      await fileRef.put(data.client.ttd);
       history.push("/dashboard");
     } catch (err) {
       console.log(err);
     }
+    // console.log(data.client.ttd);
   };
 
   return <Form postData={postHandler} />;
