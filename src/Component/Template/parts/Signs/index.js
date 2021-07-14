f9c32905e3429b8e7623ef9372302ac1d07935bb
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-07-14 13:02:44 +0700

    render signs image

--- src/Component/Template/parts/Signs/index.js
+++ src/Component/Template/parts/Signs/index.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { storage } from "../../../../firebase";
 
-function Signs() {
+function Signs({ data }) {
+  const [image, setImage] = useState("");
+
+  useEffect(() => {
+    let ref = storage.ref(`images/${data.imgName}`);
+    ref.getDownloadURL().then((url) => {
+      setImage(url);
+    });
+  }, []);
+
   return (
     <div className="sign">
       <div className="pt-buana">
-        <h4>*) PT. Buana Lintas Media</h4>
+        <h4>*{")"} PT. Buana Lintas Media</h4>
       </div>
       <div className="pt">
-        <h4>*) PT. </h4>
+        <h4>
+          *{")"} PT. {data.namaPT}
+        </h4>
       </div>
       <div className="line-pt-buana">
         <input type="text" />
       </div>
       <div className="line-pt">
-        <input type="text" />
+        {/* <input type="text" /> */}
+        <img src={image || "https://via.placeholder.com/400x300"} alt="" />
       </div>
     </div>
   );
