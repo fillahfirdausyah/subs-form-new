f21aa63e7bb582656e32db2ec1a291e25365d466
Fillah Firdausyah <fillahfirdausyah22@gmail.com>
2021-07-15 11:08:19 +0700

    get Buana Lintas Signs image

--- src/Component/Template/parts/Signs/index.js
+++ src/Component/Template/parts/Signs/index.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { storage } from "../../../../firebase";
 
-function Signs({ data }) {
+function Signs({ data, filledBy }) {
   const [image, setImage] = useState("");
+  const [buana, setBuana] = useState("");
 
   useEffect(() => {
     let ref = storage.ref(`images/${data.imgName}`);
     ref.getDownloadURL().then((url) => {
       setImage(url);
+    });
+    let ref2 = storage.ref(`images/${filledBy.imgName}`);
+    ref2.getDownloadURL().then((url) => {
+      setBuana(url);
     });
   }, []);
 
@@ -22,7 +27,7 @@ function Signs({ data }) {
         </h4>
       </div>
       <div className="line-pt-buana">
-        <input type="text" />
+        <img src={buana || "https://via.placeholder.com/400x300"} alt="" />
       </div>
       <div className="line-pt">
         {/* <input type="text" /> */}
